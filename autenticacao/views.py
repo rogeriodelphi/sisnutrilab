fe4771bded5a4b58af9db9164ed42ed0022c387b
rogeriodelphi <rogeriodelphi@gmail.com>
2022-07-08 17:52:24 -0300

    Criando a tela de login

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, reverse, redirect
 from django.http import HttpResponse
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.contrib import auth
 
 from autenticacao.utils import password_is_valid
 
@@ -11,16 +12,18 @@ def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
     elif request.method == "POST":
-        usuario = request.POST.get('usuario')
+        username = request.POST.get('usuario')
         senha = request.POST.get('senha')
-        # email = request.POST.get('email')
+        email = request.POST.get('email')
+
         confirmar_senha = request.POST.get('confirmar_senha')
+
         if not password_is_valid(request, senha, confirmar_senha):
             return reverse('cadastro')
         try:
             user = User.objects.create_user(
-                username=usuario,
-                # email=email,
+                username=username,
+                email=email,
                 password=senha,
                 is_active=False
             )
@@ -32,4 +35,19 @@ def cadastro(request):
             return redirect('/auth/cadastro')
 
 def logar(request):
-    return HttpResponse("Você está na página de logar")
+    if request.method == "GET":
+        return render(request, 'logar.html')
+    elif request.method == "POST":
+        username = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+
+        usuario = auth.authenticate(username=username, password=senha)
+
+        if not usuario:
+            messages.add_message(request, constants.ERROR, 'Username ou senha inválidos')
+            return redirect('/auth/logar')
+        else:
+            auth.login(request, usuario)
+            return redirect('/')
+        if request.user.is_authenticated:
+            return redirect('/')
