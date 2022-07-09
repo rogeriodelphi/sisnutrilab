17118aebeed19a463b140ab9fd069be44e9c11f1
rogeriodelphi <rogeriodelphi@gmail.com>
2022-07-09 03:06:37 -0300

    Criando a navbar e menu lateral da plataforma

--- plataforma/models.py
+++ plataforma/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Paciente(models.Model):
+    choices_sexo = (('F', 'Feminino'),
+                    ('M', 'Maculino'))
+    nome = models.CharField(max_length=50)
+    sexo = models.CharField(max_length=1, choices=choices_sexo)
+    idade = models.IntegerField()
+    email = models.EmailField()
+    telefone = models.CharField(max_length=19)
+    nutri = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        db_table = 'Paciente'
+        verbose_name = 'Paciente'
+        verbose_name_plural = 'Pacientes'
